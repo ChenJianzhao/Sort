210df1e7a4743fea164aadd48d47da00201e6042
chenjianzhao <j.623487211@163.com>
2017-05-31 17:49:21 +0800

    modify

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -7,31 +7,45 @@ public class QuickSort {
         int left = low;
         int right = high;
         int temp = 0;
-        if(left <= right){   //待排序的元素至少有两个的情况
 
+        //待排序的元素至少有两个的情况
+        if(left <= right){
+
+            // 三者取中寻找基准元素
             selectPivotMidOfThree(arr,low,high);
 
-            temp = arr[left];  //待排序的第一个元素作为基准元素
+            temp = arr[left];       //待排序的第一个元素作为基准元素
             while(left != right){   //从左右两边交替扫描，直到left = right
 
                 while(right > left && arr[right] >= temp)
-                    right --;        //从右往左扫描，找到第一个比基准元素小的元素
-                arr[left] = arr[right];  //找到这种元素arr[right]后与arr[left]交换
+                    right --;               //从右往左扫描，找到第一个比基准元素小的元素
+                arr[left] = arr[right];     //找到这种元素arr[right]后与arr[left]交换
 
                 while(left < right && arr[left] <= temp)
-                    left ++;         //从左往右扫描，找到第一个比基准元素大的元素
-                arr[right] = arr[left];  //找到这种元素arr[left]后，与arr[right]交换
+                    left ++;                //从左往右扫描，找到第一个比基准元素大的元素
+                arr[right] = arr[left];     //找到这种元素arr[left]后，与arr[right]交换
 
             }
-            arr[right] = temp;    //基准元素归位
+            arr[right] = temp;      //基准元素归位
 
-            agg(arr,low,high,right);
+            // 将与基准元素相等元素聚集到枢轴周围
+            gather(arr,low,high,right);
 
+            // 递归处理枢轴左右子集
             quickSort(arr,low,left-1);  //对基准元素左边的元素进行递归排序
             quickSort(arr, right+1,high);  //对基准元素右边的进行递归排序
         }
     }
 
+    /**
+     * 优化策略1：三者取中
+     * 作用：降低待排序数组本身已接近有序的影响
+     *
+     * @param arr
+     * @param low
+     * @param high
+     * @return
+     */
     public static int selectPivotMidOfThree(int arr[], int low, int high) {
 
         int mid = (low + high)/2;
@@ -54,32 +68,41 @@ public class QuickSort {
         //分割时可以直接使用low位置的元素作为枢轴，而不用改变分割函数了
     }
 
-    public static void swap(int arr[], int a, int b) {
-        int tmp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = tmp;
-    }
 
-    public static void agg(int arr[], int low, int high,int mid){
+    /**
+     * 优化策略2：聚集枢轴相同元素
+     * 作用：降低待排序数组存在大量相同元素的影响
+     *
+     * @param arr
+     * @param low
+     * @param high
+     * @param mid
+     */
+    public static void gather(int arr[], int low, int high,int mid){
+
         int left = low;
         int right = mid-1;
 
         if(left < right) {
             while(left < right ) {
 
+                // 在右边找到一个比枢轴小的元素
                 while(left < right && arr[right] == arr[mid]){
                     right--;
                 }
 
-
+                // 在左边找到一个和枢轴相等的元素
                 while(left < right && arr[left] != arr[mid]){
                     left++;
                 }
+
+                // 交换两个元素，使相同元素聚集到枢轴周围
                 if(left!=right) {
                     swap(arr,left,right);
                 }
             }
         }
+
 
         left = mid+1;
         right = high;
@@ -100,6 +123,15 @@ public class QuickSort {
                 }
             }
         }
+    }
+
+    // 优化策略3：当待排序子集缩小到一定范围后，使用插入排序效率更高
+
+
+    public static void swap(int arr[], int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
     }
 
     public static void main(String[] args) {
